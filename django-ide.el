34f6387b46d0c4dad907c2e62f0034a231aefccf
Yaroslav Shirokov <yshirokov@slavasaur.local>
2009-08-13 23:38:21 -0400

    Adding .gitignore

--- django-ide.el
+++ django-ide.el
@@ -1,6 +1,6 @@
 (defvar django-servers (make-hash-table :test 'equal))
 (defvar django-default-settings (or (getenv "DJANGO_SETTINGS_MODULE") "settings"))
-(defvar django-default-name nil)
+(defvar django-default-name "default")
 
 (defun django-ide-unload-function ()
   "Unload function for django-ide"
@@ -9,15 +9,13 @@
 
 (defun prompt-string-or-nil (prompt)
   (let* ((string (read-string (concat prompt ": ")))
-         (string (if (string= string "") nil string)))
+         (string (if (equal string "") nil string)))
     string))
 
-(defun server-buffer-name (base)
-  (let ((base (if (and (stringp base) (string= base ""))
-                  nil
-                  (and (stringp base) base)))
-        (base (or base "default")))
-    base))
+(defun server-buffer-name (prompt)
+  (let* ((name (and prompt (prompt-string-or-nil "Project name")))
+         (name (or name django-default-name)))
+    name))
 
 (defun django-settings (&optional prompt)
   (let* ((settings (and prompt (prompt-string-or-nil "Settings module")))
@@ -25,14 +23,17 @@
     settings))
 
 (defun start-or-restart-server (name settings)
-  t)
+  "django-default-server")
 
 (defun make-django-server (prefix)
   (interactive "P")
-  (let ((name (server-buffer-name django-default-name))
-        (settings (django-settings prefix)))
-    (message "Prefix: %s" prefix)
-    (message "Name: %s Settings: %s" name settings)))
+  (let* ((name (server-buffer-name prefix))
+        (settings (django-settings prefix))
+        (action (let ((result (start-or-restart-server name settings)))
+                  (cond ((equal result nil) "Failed")
+                        ((equal result t) "Restarted")
+                        ((stringp result) (concat "Started: " result))))))
+    (message "%s DEBUG: Name: %s Settings: %s" action name settings)))
 
 
 (provide 'django-ide)
